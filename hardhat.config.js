53b1fa85a9bab3d045a323ec498d67526ab5d6ef
waqeen <a.zhdanuhin@mail.ru>
2023-04-10 23:01:20 +0300

    removed keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "ad734e6063de409aa619e3ace9adaf94";
+const INFURA_API_KEY = "key";
 
 // Replace this private key with your Sepolia account private key
-// To export your private key from Metamask, open Metamask and
+// To export your private key from Metamask, open Metamask and 
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "54c1496eddb847003222ae97f63f5764447ce272dc882447a7794e54f00c0483";
-const MAINNET_PRIVATE_KEY = "54c1496eddb847003222ae97f63f5764447ce272dc882447a7794e54f00c0483";
+const SEPOLIA_PRIVATE_KEY = "key";
+const MAINNET_PRIVATE_KEY = "key";
 
 
 //import('hardhat/config.js').HardhatUserConfig;
